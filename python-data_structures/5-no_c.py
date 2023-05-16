3d63df138c4ab939b0084a9a823fefe159c135fe
raebrown-ctrl <brownrae6@gmail.com>
2023-05-16 16:35:12 -0700

    changed 5-no.c.py

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
-new_in_list = __import__('4-new_in_list').new_in_list
-
-my_list = [1, 2, 3, 4, 5]
-idx = 3
-new_element = 9
-new_list = new_in_list(my_list, idx, new_element)
-
-print(new_list)
-print(my_list)
+def no_c(my_string):
+    my_new_string = my_string.translate({ord(i): None for i in "cC"})
+    return my_new_string
