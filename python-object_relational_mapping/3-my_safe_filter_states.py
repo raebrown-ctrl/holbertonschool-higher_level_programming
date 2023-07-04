11bcd40f6afd6f8cd94808dfd7a9a51e233b772f
raebrown-ctrl <brownrae6@gmail.com>
2023-07-04 09:21:55 -0700

    sql injection

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+lists all states with name from the database
+"""
+import MySQLdb
+from sys import argv
+
+
+def main(username, passwd, db, statename):
+    """
+    list  where name matches the argument.
+    """
+    conn = MySQLdb.connect(
+        host='localhost',
+        user=username,
+        password=passwd,
+        database=db,
+        port=3306
+    )
+    """
+    CREATE A CURSOR OBJECT FOR EXECUTE ALL THE QUERIES WE NEED IT
+    """
+    c = conn.cursor()
+    c.execute("""SELECT *
+                FROM states WHERE name=%s
+                ORDER BY id ASC """, (statename, ))
+
+    rows = c.fetchall()
+    for eachRow in rows:
+        print(eachRow)
+
+
+if __name__ == '__main__':
+    main(argv[1], argv[2], argv[3], argv[4])
