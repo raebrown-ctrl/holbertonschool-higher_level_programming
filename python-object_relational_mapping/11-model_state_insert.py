f695d4b670af512e1df27b3e5ad5c1ff87d325a6
raebrown-ctrl <brownrae6@gmail.com>
2023-07-04 09:53:03 -0700

    state insert

---
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+update state
+"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+
+    # make engine
+    user = argv[1]
+    passwd = argv[2]
+    db = argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(user, passwd, db), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # add new state
+    new = State(name="Louisiana")
+    session.add(new)
+    session.commit()
+
+    print("{:d}".format(new.id))
+
+    session.close()
